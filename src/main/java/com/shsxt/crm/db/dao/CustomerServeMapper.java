b4d1e86102307cd7d1ff0b4b91f4e217f0e240cc
jojo <18260632886@163.com>
2019-12-19 14:01:58 +0800

    服务管理模块——服务的 创建和分配

---
+++ src/main/java/com/shsxt/crm/db/dao/CustomerServeMapper.java
@@ -0,0 +1,7 @@
+package com.shsxt.crm.db.dao;
+
+import com.shsxt.base.BaseMapper;
+import com.shsxt.crm.vo.CustomerServe;
+
+public interface CustomerServeMapper extends BaseMapper<CustomerServe, Integer> {
+}
